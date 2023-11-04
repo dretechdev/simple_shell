40b1ea79c2156c877b89ed31edf642097a7ee438
ousmaneMano <ousmanemanot@gmail.com>
2023-11-03 18:30:12 -0700

    task 1

--- _atoi.c
+++ _atoi.c
@@ -1,1 +1,75 @@
-P
+#include "main.h"
+
+/**
+ * interactive - returns true if shell is interactive mode.
+ * @info: struct address.
+ * Return: 1 if interactive mode, otherwise 0.
+ */
+int interactive(info_t *info)
+{
+	return (isatty(STDIN_FILENO) && info->readfd <= 2);
+}
+
+/**
+ * is_delimeter - checks if char is a delimeter.
+ * @c: the char to check.
+ * @delim: the delimeter string.
+ * Return: 1 if true, 0 if false
+ */
+int is_delimeter(char c, char *delim)
+{
+	while (*delim)
+		if (*delim++ == c)
+		{
+			return (1);
+		}
+	return (0);
+}
+
+/**
+ * is_alpha - checks for alphabetic character.
+ * @c: input character.
+ * Return: 1 if c is alphabetic, 0 otherwise
+ */
+
+int is_alpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
+
+/**
+ * _atoi - converts a string to an int.
+ * @s: the string to be converted.
+ * Return: 0 if no numbers in string, converted number otherwise.
+ */
+
+int _atoi(char *s)
+{
+	int i, sig = 1, fg = 0, outp;
+	unsigned int result = 0;
+
+	for (i = 0;  s[i] != '\0' && fg != 2; i++)
+	{
+		if (s[i] == '-')
+			sig *= -1;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			fg = 1;
+			result *= 10;
+			result += (s[i] - '0');
+		}
+		else if (fg == 1)
+			fg = 2;
+	}
+
+	if (sig == -1)
+		outp = -result;
+	else
+		outp = result;
+
+	return (outp);
+}
---
+++ builtin.c
@@ -0,0 +1,93 @@
+#include "main.h"
+
+/**
+ * my_exit - exits the shell.
+ * @info: Structure containing potential arguments.
+ *  Return: exit
+ *         (0) if info.argv[0] != "exit"
+ */
+int my_exit(info_t *info)
+{
+	int excheck;
+
+	if (info->argv[1])
+	{
+		excheck = _erratoi(info->argv[1]);
+		if (excheck == -1)
+		{
+			info->status = 2;
+			print_error(info, "Illegal number: ");
+			_eputs(info->argv[1]);
+			_eputchar('\n');
+			return (1);
+		}
+		info->err_num = _erratoi(info->argv[1]);
+		return (-2);
+	}
+	info->err_num = -1;
+	return (-2);
+}
+
+/**
+ * my_cd - changes the current directory of the process
+ * @info: Structure containing potential arguments.
+ *  Return: 0
+ */
+int my_cd(info_t *info)
+{
+	char *s, *dir, buffer[1024];
+	int chd_ret;
+
+	s = getcwd(buffer, 1024);
+	if (!s)
+		_puts("TODO: >>getcwd failure emsg here<<\n");
+	if (!info->argv[1])
+	{
+		dir = get_env(info, "HOME=");
+		if (!dir)
+			chd_ret = chdir((dir = get_env(info, "PWD=")) ? dir : "/");
+		else
+			chd_ret = chdir(dir);
+	}
+	else if (_strcmp(info->argv[1], "-") == 0)
+	{
+		if (!get_env(info, "OLDPWD="))
+		{
+			_puts(s);
+			_putchar('\n');
+			return (1);
+		}
+		_puts(get_env(info, "OLDPWD=")), _putchar('\n');
+		chd_ret = chdir((dir = get_env(info, "OLDPWD=")) ? dir : "/");
+	}
+	else
+		chd_ret = chdir(info->argv[1]);
+	if (chd_ret == -1)
+	{
+		print_error(info, "can't cd to ");
+		_eputs(info->argv[1]), _eputchar('\n');
+	}
+	else
+	{
+		set_env(info, "OLDPWD", get_env(info, "PWD="));
+		set_env(info, "PWD", getcwd(buffer, 1024));
+	}
+	return (0);
+}
+
+/**
+ * my_help - changes the current directory
+ * @info: Structure containing potential arguments.
+ *  Return: 0
+ */
+int my_help(info_t *info)
+{
+	char **arg_arr;
+
+	arg_arr = info->argv;
+	_puts("help call works. Function not yet implemented \n");
+	if (0)
+		_puts(*arg_arr);
+	return (0);
+}
+
